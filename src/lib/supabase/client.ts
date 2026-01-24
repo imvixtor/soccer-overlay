a5c13f13f5b8941cefa5ac1f30efd48053223233
Lại Quân <quanlb.b21cn607@stu.ptit.edu.vn>
2026-01-24 23:56:51 +0700

    feat: add supabase types

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,7 +1,8 @@
 import { createClient as createSupabaseClient } from '@supabase/supabase-js';
+import type { Database } from '@/types/supabase';
 
 export function createClient() {
-    return createSupabaseClient(
+    return createSupabaseClient<Database>(
         import.meta.env.VITE_SUPABASE_URL!,
         import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY!,
     );
